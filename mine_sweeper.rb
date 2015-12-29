0e2df926d160c39c7f0a56fef250bb2e3fcb362c
github username <mendelblesofsky@gmail.com>
2015-12-29 15:14:55 -0500

    the towns-people wept, as the mines were swept.

--- mine_sweeper.rb
+++ mine_sweeper.rb
@@ -1,13 +1,29 @@
-class MineSweeper
-  
+class MineSweeper  
   # attrs here
+  attr_accessor :field
   
   def initialize(field)
     # code here
+    @field = field
   end
 
   def sweep_field
     # code here
+    field.each_with_index do |arr, index|
+    	arr.each_with_index do |e, i|
+    		sweep_row(index, i) if e == '*'
+    	end
+    end
+    field
   end
 
+  def sweep_row(i1, i2)
+  	[i1-1, i1, i1+1].each do |row|
+	  	[i2-1,i2,i2+1].each do |i|
+	  		if row != -1 && field[row] && field[row][i] && field[row][i] != '*'
+	  			field[row][i] += 1
+	  		end
+	  	end
+	end
+  end
 end
